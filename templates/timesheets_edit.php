d0e896308d8ec67908336277e4b6ae06558b9b41
Remco Tolsma <info@remcotolsma.nl>
2013-12-24 10:03:55 +0100

    Only show form if the entry to edit was found.

--- templates/timesheets_edit.php
+++ templates/timesheets_edit.php
@@ -10,29 +10,32 @@ $id = filter_input( INPUT_GET, 'entry_id', FILTER_SANITIZE_STRING );
 
 $entry = orbis_timesheets_get_entry( $id );
 
-?>
-<div class="panel">
-	<div class="content">
-		<form action="" method="post">
-			<?php wp_nonce_field( 'orbis_timesheets_add_new_registration', 'orbis_timesheets_new_registration_nonce' ); ?>
-
-			<legend><?php _e( 'Edit registration', 'orbis_timesheets' ); ?></legend>
-
-			<?php include 'work_registration_fields.php'; ?>
-
-			<div class="form-actions">
-				<button type="submit" class="btn btn-primary" name="orbis_timesheets_add_registration"><?php esc_html_e( 'Edit', 'orbis_timesheets' ); ?></button>
-				
-				<?php 
-
-				$cancel_url = add_query_arg( array(
-					'work_registration' => false,
-					'action'            => false,
-				) );
-
-				?>
-				<a class="btn" href="<?php echo esc_attr( $cancel_url ); ?>"><?php esc_html_e( 'Cancel', 'orbis_timesheets' ); ?></a>
-			</div>
-		</form>
+if ( $entry ) : ?>
+	
+	<div class="panel">
+		<div class="content">
+			<form action="" method="post">
+				<?php wp_nonce_field( 'orbis_timesheets_add_new_registration', 'orbis_timesheets_new_registration_nonce' ); ?>
+	
+				<legend><?php _e( 'Edit registration', 'orbis_timesheets' ); ?></legend>
+	
+				<?php include 'work_registration_fields.php'; ?>
+	
+				<div class="form-actions">
+					<button type="submit" class="btn btn-primary" name="orbis_timesheets_add_registration"><?php esc_html_e( 'Edit', 'orbis_timesheets' ); ?></button>
+					
+					<?php 
+	
+					$cancel_url = add_query_arg( array(
+						'work_registration' => false,
+						'action'            => false,
+					) );
+	
+					?>
+					<a class="btn" href="<?php echo esc_attr( $cancel_url ); ?>"><?php esc_html_e( 'Cancel', 'orbis_timesheets' ); ?></a>
+				</div>
+			</form>
+		</div>
 	</div>
-</div>
+
+<?php endif; ?>
