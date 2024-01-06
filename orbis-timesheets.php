ec1757c16ca197fd4f5cf8ca8652acd5a155f8f2
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-06 13:33:03 +0100

    Update

--- orbis-timesheets.php
+++ orbis-timesheets.php
@@ -1,22 +1,30 @@
 <?php
-/*
-Plugin Name: Orbis Timesheets
-Plugin URI: http://www.orbiswp.com/
-Description: Time Management, Timesheet, Time Tracking solution for WordPress. Orbis Timesheets enables you to track your work time.
+/**
+ * Orbis Timesheets
+ *
+ * @package   Pronamic\Orbis\Timesheets
+ * @author    Pronamic
+ * @copyright 2024 Pronamic
+ * @license   GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Orbis Timesheets
+ * Plugin URI:        https://wp.pronamic.directory/plugins/orbis-timesheets/
+ * Description:       Time Management, Timesheet, Time Tracking solution for WordPress. Orbis Timesheets enables you to track your work time.
+ * Version:           1.3.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pronamic
+ * Author URI:        https://www.pronamic.eu/
+ * Text Domain:       orbis-timesheets
+ * Domain Path:       /languages/
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wp.pronamic.directory/plugins/orbis-timesheets/
+ * GitHub URI:        https://github.com/pronamic/wp-orbis-timesheets
+ */
 
-Version: 1.3.3
-Requires at least: 3.5
-
-Author: Pronamic
-Author URI: http://www.pronamic.eu/
-
-Text Domain: orbis_timesheets
-Domain Path: /languages/
-
-License: GPLv3
-
-GitHub URI: https://github.com/pronamic/wp-orbis-subscriptions
-*/
+namespace Pronamic\Orbis\Timesheets;
 
 /**
  * Autoload.
@@ -36,7 +44,7 @@ function orbis_timesheets_bootstrap() {
 	// Initialize
 	global $orbis_timesheets_plugin;
 
-	$orbis_timesheets_plugin = new Orbis_Timesheets_Plugin( __FILE__ );
+	$orbis_timesheets_plugin = new \Orbis_Timesheets_Plugin( __FILE__ );
 }
 
 add_action( 'orbis_bootstrap', 'orbis_timesheets_bootstrap' );
