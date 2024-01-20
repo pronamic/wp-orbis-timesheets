3e826b25aedb5279f3a073537501fcc477711f01
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-20 13:51:53 +0100

    Create monthly-report.php

---
+++ templates/monthly-report.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Monthly report
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2024 Pronamic
+ * @license   GPL-2.0-or-later
+ * @package   Pronamic\Orbis\Tasks
+ */
+
+get_header();
+
+?>
+<div>
+	Maandrapport
+</div>
+<?php
+
+get_footer();
