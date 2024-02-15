d45a01dffd8f8c5452bf6e0e71e25a4c8c82a8f3
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-02-15 12:20:48 +0100

    Create query-projects-time-left-2023.sql

---
+++ examples/query-projects-time-left-2023.sql
@@ -0,0 +1,48 @@
+SELECT
+	company.id AS company_id,
+	company.name AS company_name,
+	project.id AS project_id,
+	project.name AS project_name,
+	project.start_date AS project_start_date,
+	project.invoice_number AS project_invoice_number,
+	project.number_seconds AS project_seconds,
+	TIME_FORMAT( SEC_TO_TIME( project.number_seconds ),'%H:%i' ) AS project_time,
+	summary.seconds_spent AS project_seconds_spent,
+	TIME_FORMAT( SEC_TO_TIME( summary.seconds_spent ),'%H:%i' ) AS project_time_spent,
+	project.number_seconds - summary.seconds_spent AS project_seconds_left,
+	TIME_FORMAT( SEC_TO_TIME( project.number_seconds - summary.seconds_spent ), '%H:%i' ) AS project_time_left
+FROM
+	wp_orbis_projects AS project
+		LEFT JOIN
+	wp_orbis_companies AS company
+			ON project.principal_id = company.id
+		LEFT JOIN
+	(
+		SELECT
+			project.id AS project_id,
+			COALESCE( SUM( timesheet.number_seconds ), 0 ) AS seconds_spent
+		FROM
+			wp_orbis_projects AS project
+				LEFT JOIN
+			wp_orbis_hours_registration AS timesheet
+					ON (
+						project.id = timesheet.project_id
+							AND
+						timesheet.date <= '2023-12-31'
+					)
+		GROUP BY
+			project.id
+	) AS summary
+			ON project.id = summary.project_id
+WHERE
+	project.invoice_number LIKE '23%'
+		AND
+	(
+		project.name LIKE '%Strippenkaart%'
+	)
+		AND
+	project.invoicable
+GROUP BY
+	project.id
+		HAVING ( project.number_seconds - summary.seconds_spent ) > 0
+;
